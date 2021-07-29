b044b87366f45b2a8f5c01f83edd07da85f50222
Alexander Skiba <skibaalex1@gmail.com>
2021-07-29 11:19:17 -0500

    added error handling routes

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const { errorMonitor } = require('events');
+const {
+    errorMonitor
+} = require('events');
 const express = require('express');
 const path = require('path');
 const data = require('./data.json')
@@ -7,15 +9,15 @@ const app = express()
 app.set('view engine', 'pug')
 app.use('/static', express.static(path.join(__dirname, 'public')))
 
-app.use((err, req, res, next) => {
-    if (!err.status) err.status = 404
-    if (!err.message) err.message = 'Opps something went wrong'
-    next()
-})
+
 
 app.get('/', (req, res) => {
-    const { projects } = data
-    res.render('index', { projects })
+    const {
+        projects
+    } = data
+    res.render('index', {
+        projects
+    })
 })
 
 app.get('/about', (req, res) => {
@@ -23,19 +25,28 @@ app.get('/about', (req, res) => {
 })
 
 app.get('/project/:id', (req, res) => {
-    const { id } = req.params
+    const {
+        id
+    } = req.params
     const project = data.projects[id]
-    res.render('project', { project })
+    res.render('project', {
+        project
+    })
 
 })
 
 app.use((req, res, next) => {
     res.status(404)
     const err = new Error('404 Page was not found')
+    res.render('page-not-found')
     next(err)
 })
 
-
+app.use((err, req, res, next) => {
+    if (!err.status) err.status = 500
+    if (!err.message) err.message = 'Opps something went wrong'
+    res.render('error')
+})
 
 
 const port = process.env.PORT || 3000;
